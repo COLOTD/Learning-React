fe0e1eb59dba84b7b58f5a9cdfc18439f4f01804
Mollazehi44kosar <mollazehi44kosar@gmail.com>
2026-06-04 17:12:07 +0330

    first commit

---
+++ vite-project/src/main.jsx
@@ -0,0 +1,17 @@
+
+import { createRoot } from 'react-dom/client'
+import './index.css'
+
+let button= "buttton"
+createRoot(document.getElementById('root')).render(
+  <div>
+    {button}
+
+    <span>
+        sisi
+    </span>
+
+  </div>
+
+ 
+)
