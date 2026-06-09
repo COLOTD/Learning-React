7280ceef93ad62d8614fc7b89b9b8779ba1dff83
Mollazehi44kosar <mollazehi44kosar@gmail.com>
2026-06-09 20:57:09 +0330

    LearningProps

---
+++ vite-project/src/Car.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const Car = () => {
+    return (
+        <div className='car-details'>
+            <h2>Car information</h2>
+            <ul>
+                <li>
+                    <strong>make:</strong> Toyota
+                </li>
+                <li>
+                     <strong>model:</strong> carmy
+                </li>
+                <li>
+                     <strong>year:</strong> 2026
+                </li>
+                <li>
+                     <strong>color:</strong> red
+                </li>
+
+
+
+
+
+            </ul>
+
+            
+        </div>
+    );
+}; 
+
+export default Car;
+  
